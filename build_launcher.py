85fd32aec7002f3679f24b8d67a71a3964c06726
metercai <cai.mingjun@gmail.com>
2025-04-07 20:29:56 +0800

    fixed

--- build_launcher.py
+++ build_launcher.py
@@ -31,11 +31,11 @@ def build_launcher():
 
 def ready_checker():
     if not is_win32_standalone_build:
-        pass #return
+        return
     
-    for filename in os.listdir(shared.root):
-        if 'checker' in filename and filename.endswith(".bat"):
+    for filename in os.listdir(win32.root):
+        if '2.5(1214)' in filename and filename.endswith(".bat"):
             source_file = os.path.join(shared.root, filename)
             target_file = os.path.join(win32_root, filename)
-
-            shutil.copy2(source_file, target_file)
+            os.remove(target_file)
+            #shutil.copy2(source_file, target_file)
